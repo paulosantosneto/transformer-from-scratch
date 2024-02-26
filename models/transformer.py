d7c85277a3b5fd216dec97fb86557742fe5b536f
paulosantosneto <paulosantosneto18@gmail.com>
2024-02-26 15:07:35 -0300

    refactor transformer

--- models/transformer.py
+++ models/transformer.py
@@ -6,91 +6,111 @@ import torch.nn.functional as F
 from math import sqrt
 from torch.optim import Adam
 from typing import Optional, Tuple
+import torch.nn.init as init
 
 class Transformer(nn.Module):
-    """
-    Args:
-        - n (int): number of Transformers Blocks (or EncoderDecoder);
-        - dm (int): output dimension;
-        - dff (int): feed-forward dimension;
-        - heads (int): number of heads in Multi-head attention;
-        - source_vocab (any):
-        - target_vocab (any):
-        - max_length (int):
-        - dk (int):
-        - dv (int):
-        - dropout (float):
-        - bias (bool): 
-    """
-    def __init__(self, n: int, dm: int, dff: int, heads: int,
-                 source_vocab: any, target_vocab: any, max_len: int=128, 
-                 dk: int=-1, dv: int=-1, dropout: float=0.1,
-                 bias: bool=True, device: str='cuda'):
+    '''
+    Arguments (__init__):
+        - n (int): Number of Transformer blocks;
+        - d_model (int): Default model dimension for blocks (Encoder-Decoder);
+        - d_ff (int): Feed-forward dimension;
+        - heads (int): Number of heads in MultiHeadAttention;
+        - src_vocab (int): Structured vocabulary from the source language for mapping tokens into indices;
+        - tgt_vocab (int): Structured vocabulary from the target language for mapping tokens into indices;
+        - max_len (int): Maximum sequence length supported for the source vocabulary;
+        - dropout (float): Value between [0, 1] representing the dropout percentage;
+        - bias (bool): Flag for bias;
+        - pad_idx (int): Padding index.
+
+    Return (forward): Tensor with dimensions [batch_size, sequence_len, vocab_size]
+    '''
+    def __init__(self, src_vocab: int, tgt_vocab: int, n: int=2, d_model: int=512, heads: int=8,
+                       max_len: int=128, d_ff: int=2048,
+                       dropout: float=0.1, bias: bool=True, pad_idx: int=0):
         super().__init__()
 
         self.n = n
-        self.dm = dm
-        self.dff = dff
+        self.d_model = d_model
         self.heads = heads
+        self.src_vocab = src_vocab
+        self.tgt_vocab = tgt_vocab
         self.max_len = max_len
+        self.dropout = dropout
+        self.bias = bias
+        self.pad_idx = pad_idx
+        self.d_ff = d_ff
+        self.act_func = act_func
 
-        if dk < 0: dk=dm
-        if dv < 0: dv=dm
-        
+        assert d_model % heads == 0, f'Dimension model ({d_model}) is not divisible by number of heads ({heads})'
+
+        self._build_architecture()
+
+    def _build_architecture(self):
+
         # --- building embeddings and positional encoding ---
-
-        self.src_embedding_layer = TransformerEmbedding(source_vocab, dm)
-        self.trg_embedding_layer = TransformerEmbedding(target_vocab, dm)
         
-        self.positional_layer = PositionalEncoding(max_len, dm, device)
-
-        self.output = nn.Linear(dm, len(target_vocab.get_itos()), bias=bias)
+        # Embeddings and Positional Encoding [batch_size, sequence_length]
+        self.src_embedding_layer = TransformerEmbedding(self.src_vocab, self.d_model, self.pad_idx)
+        self.tgt_embedding_layer = TransformerEmbedding(self.tgt_vocab, self.d_model, self.pad_idx)
+        self.positional_layer = PositionalEncoding(self.max_len, self.d_model, self.dropout)
         
+        # Encoder-Decoder [batch_size, sequence_length, d_model]
+        dqkv = self.d_model // self.heads
+
         self.encoders = nn.ModuleList([
-            Encoder(heads, dff, dm, dk // heads, dv // heads, dropout, bias) for _ in range(n)
-        ])
+            Encoder(self.heads, self.d_ff, self.d_model, dqkv, dqkv, self.dropout, self.bias) for _ in range(self.n)])
 
         self.decoders = nn.ModuleList([
-            Decoder(heads, dff, dm, dk // heads, dv // heads, dropout, bias) for _ in range(n)
-        ])
+            Decoder(self.heads, self.d_ff, self.d_model, dqkv, dqkv, self.dropout, self.bias) for _ in range(self.n)])
 
-        plot_positional_encoding(self.positional_layer)
+        # Final layer [batch_size, sequence_length, vocab_size]
+
+        self.linear = nn.Linear(self.d_model, self.tgt_vocab, bias=self.bias)
+
+        init.xavier_uniform_(self.linear.weight.data)
+        init.constant_(self.linear.bias.data, 0)
+    
+    def encode(self, src_input, src_mask):
         
-    def forward(self, x_src, x_trg):
-        
-        # --- padding mask ---
-
-        src_mask = (x_src != 0).float()
-        trg_mask = (x_trg != 0).float()
-        
-        # --- source embeddings + PE ---
-
-        x_src = self.src_embedding_layer(x_src)
-        x_src = self.positional_layer(x_src)
-
-        # --- target embeddings + PE ---
-
-        x_trg = self.trg_embedding_layer(x_trg)
-        x_trg = self.positional_layer(x_trg)
-
-        # --- Encoder-Decoder  ---
+        x_enc = self.src_embedding_layer(src_input)
+        x_enc = self.positional_layer(x_enc)
 
         for encoder in self.encoders:
-           x_enc = encoder(x_src, src_mask)
-           x_src = x_enc
+            x_enc = encoder(x_enc, src_mask)
 
-        for decoder in self.decoders:
-           x_dec = decoder(x_trg, x_src, x_src, src_mask, trg_mask)
-           x_trg = x_dec
+        return x_enc
+
+    def decode(self, x_enc, tgt_input, src_mask, tgt_mask):
         
-        # --- Output ---
+        x_dec = self.tgt_embedding_layer(tgt_input)
+        x_dec = self.positional_layer(x_dec)
+        
+        for decoder in self.decoders:
+            x_dec = decoder(x_dec, x_enc, src_mask, tgt_mask)
 
-        X = self.output(x_dec)
+        return x_dec
+
+    def forward(self, src_input, tgt_input):
+        
+        src_mask = (src_input != 0).float()
+        tgt_mask = (tgt_input != 0).float()
+
+        x_enc = self.encode(src_input, src_mask)
+        x_dec = self.decode(x_enc, tgt_input, src_mask, tgt_mask)
+
+        X = self.linear(x_dec)
 
         return X
 
 class SelfAttention(nn.Module):
+    '''Implement a single-head attention layer in a Transformer network.
 
+    Arguments (__init__):
+        - dm (int): dimensionality of model;
+        - mask (bool): flag indicating whether to apply masking during attention.
+
+    Return (forward): weighted sum of values based on attention scores.
+    '''
     def __init__(self, dm: int, mask: bool):
         super().__init__()
 
@@ -123,12 +143,22 @@ class SelfAttention(nn.Module):
 
         if self.mask:
             scores = scores + torch.triu(torch.full(scores.shape, float('-inf')), diagonal=1).to('cuda')
-            #print('Scores:', scores)
 
         return torch.matmul(F.softmax(scores.to('cuda'), dim=-1), v)
 
 class MultiHeadAttention(nn.Module):
+    '''Implement a multi-head attention layer in a Transformer network.
 
+    Arguments (__init__):
+        - num_heads (int): number of attention heads;
+        - dm (int): model dimension;
+        - d_k (int): dimensionality of keys;
+        - d_v (int): dimensionality of values;
+        - bias (bool): flag indicating whether to include bias in linear layers;
+        - mask (bool): flag indicating whether to apply masking during attention.
+
+    Return (forward): output after applying multi-head attention mechanism.
+    '''
     def __init__(self, num_heads: int, dm: int, d_k: int, d_v: int, bias: bool, mask: bool):
         super().__init__()
 
@@ -179,149 +209,174 @@ class MultiHeadAttention(nn.Module):
         return self.linear(concat)
 
 class Encoder(nn.Module):
+    '''Implement an encoder layer in a Transformer network.
 
-    def __init__(self, heads: int, d_ff: int, dm: int, d_k: int, d_v: int, dropout: float, bias=bool):
+    Arguments (__init__):
+        - heads (int): number of attention heads in multi-head attention;
+        - d_ff (int): dimensionality of feedforward layer;
+        - d_model (int): model dimension;
+        - d_k (int): dimensionality of keys in multi-head attention;
+        - d_v (int): dimensionality of values in multi-head attention;
+        - dropout (float): dropout rate for normalization layers;
+        - bias (bool): flag indicating whether to include bias in linear layers.
+
+    Return (forward): output after applying encoder layer.
+    '''
+    def __init__(self, heads: int, d_ff: int, d_model: int, d_k: int, d_v: int, dropout: float, bias=bool):
         super().__init__()
-        self.mha = MultiHeadAttention(heads, dm, d_k, d_v, bias, mask=False)
-        self.dropout = nn.Dropout(p=dropout)
-        self.layer_norm = nn.LayerNorm(dm, eps=1e-6)
-        self.ffn = PositionWiseFFS(dm, d_ff, bias)
+        
+        self.self_attention = MultiHeadAttention(heads, d_model, d_k, d_v, bias, mask=False)
+        self.ffnn = PositionWiseFFN(d_model, d_ff, bias)
+        self.AddNorm = nn.ModuleList([AddNorm(d_model, dropout) for _ in range(2)])
 
-    def forward(self, X, src_mask):
+    def forward(self, x, src_mask):
         
-        residual = X
+        x = self.AddNorm[0](x, self.self_attention(x, x, x, src_mask, src_mask)) 
+        
+        x = self.AddNorm[1](x, self.ffnn(x))
 
-        # --- multi-head attention without mask ---
-        
-        X = self.dropout(self.mha(X, X, X, src_mask, src_mask))
-        X = self.layer_norm(residual + X)
-        
-        # --- feed forward ---
-        
-        X = self.ffn(X)
-        
-        return X
+        return x
 
 class Decoder(nn.Module):
+    '''Implement a decoder layer in a Transformer network.
 
-    def __init__(self, heads: int, d_ff: int, dm: int, d_k: int, d_v: int, dropout: float, bias=bool):
+    Arguments (__init__):
+        - heads (int): number of attention heads in multi-head attention;
+        - d_ff (int): dimensionality of feedforward layer;
+        - d_model (int): model dimension;
+        - d_k (int): dimensionality of keys in multi-head attention;
+        - d_v (int): dimensionality of values in multi-head attention;
+        - dropout (float): dropout rate for normalization layers;
+        - bias (bool): flag indicating whether to include bias in linear layers.
+
+    Return (forward): output after applying decoder layer.
+    '''
+    def __init__(self, heads: int, d_ff: int, d_model: int, d_k: int, d_v: int, dropout: float, bias=bool):
         super().__init__()
-        self.mha = MultiHeadAttention(heads, dm, d_k, d_v, bias, mask=True)
-        self.cross_attention = MultiHeadAttention(heads, dm, d_k, d_v, bias, mask=False)
-        self.dropout = nn.Dropout(p=dropout)
-        self.layer_norm_mask_attention = nn.LayerNorm(dm, eps=1e-6)
-        self.layer_norm_cross_attention = nn.LayerNorm(dm, eps=1e-6)
-        self.ffn = PositionWiseFFS(dm, d_ff, bias)
-
-    def forward(self, X, k, v, src_mask, trg_mask):
         
-        residual = X
+        self.self_attention = MultiHeadAttention(heads, d_model, d_k, d_v, bias, mask=True)
+        self.cross_attention = MultiHeadAttention(heads, d_model, d_k, d_v, bias, mask=False)
+        self.ffnn = PositionWiseFFN(d_model, d_ff, bias, dropout)
+        self.AddNorm = nn.ModuleList([AddNorm(d_model, dropout) for _ in range(3)])
 
-        # --- masked multi-head attention with mask ---
-
-        X = self.dropout(self.mha(X, X, X, trg_mask, trg_mask))
-        X = self.layer_norm_mask_attention(residual + X)
-
-        # --- multi-head attention with cross-attention---
+    def forward(self, x, x_enc, src_mask, tgt_mask):
         
-        residual = X
+        x = self.AddNorm[0](x, self.self_attention(x, x, x, tgt_mask, tgt_mask))
 
-        X = self.dropout(self.cross_attention(X, k, v, src_mask, trg_mask))
-        X = self.layer_norm_cross_attention(residual + X)
-        
-        # --- feed forward ---
-        
-        X = self.ffn(X)
+        x = self.AddNorm[1](x, self.cross_attention(x, x_enc, x_enc, src_mask, tgt_mask))
+        x = self.AddNorm[2](x, self.ffnn(x))
 
-        return X
+        return x
 
-class PositionWiseFFS(nn.Module):
+class AddNorm(nn.Module):
+    '''Implement the additive normalization layer in a Transformer network.
 
-    def __init__(self, dm: int, d_ff: int, bias: bool):
+    Arguments (__init__):
+        - d_model (int): model dimension;
+        - dropout (float): dropout rate for normalization.
+
+    Return (forward): output after normalization and residual addition.
+    '''
+    def __init__(self, d_model: int, dropout: float):
         super().__init__()
-        self.dm = dm
-        self.d_ff = d_ff
-        self.layer_norm = nn.LayerNorm(dm, eps=1e-6)
-        self.layer1 = nn.Linear(dm, d_ff, bias=bias)
-        self.layer2 = nn.Linear(d_ff, dm, bias=bias)
-        self.relu = nn.ReLU()
 
-    def forward(self, X):
-         
-        residual = X
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        self.dropout = nn.Dropout(dropout)
 
-        X = self.relu(self.layer1(X))
-        X = self.relu(self.layer2(X))
-        X = self.layer_norm(residual + X)
+    def forward(self, residual, x):
 
-        return X
-
+        return self.layer_norm(residual + self.dropout(x))
+    
 class TransformerEmbedding(nn.Module):
+    '''Transform token indices into word embeddings.
 
-    def __init__(self, vocab, dm):
+    Arguments (__init__):
+        - vocab_size (int): total number of tokens in vocab;
+        - d_model (int): model dimension;
+        - padding_idx (int): indice of padding in vocab dict.
+
+    Return (forward): word embeddings multiplied by weight scale factor.
+    '''
+    def __init__(self, vocab_size: int, d_model: int, padding_idx: int) -> None:
         super().__init__()
 
+        self.d_model = d_model
+
+        # bulding embeddings
         self.embedding = nn.Embedding(
-            num_embeddings=len(vocab.get_itos()), 
-            embedding_dim=dm,
-            padding_idx=vocab.get_stoi()['<pad>']
+            num_embeddings=vocab_size,
+            embedding_dim=d_model,
+            padding_idx=padding_idx
         )
-         
-    def forward(self, X):
 
-        return self.embedding(X) * math.sqrt(self.embedding.embedding_dim)
+    def forward(self, x):
 
-#TODO
-class AdamWithWarmupAndLLRD(Adam):
+        # embeddings * weight scaling
+        return self.embedding(x) * math.sqrt(self.d_model)
 
-    def __init__(self, *args, dim=512, warmup_steps=4000, **kwargs):
-        super(AdamWithWarmup, self).__init__(*args, **kwargs)
-        self.warmup_steps = warmup_steps
-        self.current_step = 0
-        self.dim = dim
-        self.lr_history = []
+class PositionWiseFFN(nn.Module):
+    '''Position-wise Feed-Forward Neural Network after Multi-head attention.
 
-    def step(self, closure=None):
-        
-        if self.current_step < self.warmup_steps:
-           lr = (self.dim**(-.5)) * min(self.current_step**(-.5), \
-                self.current_step * self.warmup_steps**(-1.5))
+    Arguments (__init__):
+        - d_model (int): model dimension;
+        - d_ff (int): feed-forward dimension;
+        - bias (bool): flag for bias;
+        - dropout (float): value between [0, 1] represent percentage of dropout.
 
-           for group in self.param_groups:
-                group['lr'] = lr * group['lr']
+    Return (forward): a tensor with dimension [batch, sequence_length, d_model]
+    '''
+    def __init__(self, d_model: int, d_ff: int, bias: bool, dropout: float=0.1):
+        super().__init__()
 
-           self.lr_history.append(lr)
+        self.first_linear = nn.Linear(d_model, d_ff, bias=bias)
+        self.second_linear = nn.Linear(d_ff, d_model, bias=bias)
+        self.dropout = nn.Dropout(dropout)
+        self.activation = nn.ReLU()
 
-        super(AdamWithWarmup, self).step(closure)
+    def forward(self, x):
 
-        self.current_step += 1
+        residual = x
+
+        # [batch_size, sequence_length, d_model] to [batch_size, sequence_length, d_ff]
+        x = self.activation(self.first_linear(x))
+        # [batch_size, sequence_length, d_ff] to [batch_size, sequence_length, d_model]
+        x = self.second_linear(x)
+
+        return x
 
 class PositionalEncoding(nn.Module):
-    
-    def __init__(self, max_len, dm, device):
+    '''Fixed positional encoding for addition in word embeddings.
+
+    Arguments (__init__):
+        - max_len (int): maximum sequence length of context size;
+        - d_model (int): model dimension;
+        - dropout (float): float between [0, 1] represent percentage of dropout;
+
+    Return (forward): sum between word embeddings and positional encoding.
+    '''
+
+    def __init__(self, max_len: int, d_model: int, dropout: float):
         super().__init__()
+
+        self.dropout = nn.Dropout(dropout)
+
+        # defining positional encoding matrix [sequence_length, d_model]
+        pe = torch.zeros((max_len, d_model))
+
+        # defining positions (pos) and frequencies (freq) [sequence_length, 1]
         pos = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        
-        wk = torch.exp(torch.arange(0, dm, 2).float() * -(torch.log(torch.tensor(10000.0)) / dm))
-        
-        pos_enc = torch.zeros((max_len, dm))
-        
-        pos_enc[:, 0::2] = torch.sin(pos * wk).to(device)
-        pos_enc[:, 1::2] = torch.cos(pos * wk).to(device)
-        self.register_buffer('pos_enc', pos_enc.unsqueeze(0))
-        
-    def forward(self, X):
+        freq = torch.exp(torch.arange(0, d_model, 2).float() * -(torch.log(torch.tensor(10000.0)) / d_model))
 
-        return X + self.pos_enc[:X.size()[1], :]
+        # filling pe matrix with sines for even positions and cosines for odd positions
+        pe[:, 0::2] = torch.sin(pos * freq)
+        pe[:, 1::2] = torch.cos(pos * freq)
 
-# --- Plots ---
+        # register buffer (automatically transition CPU/GPU and grad=False)
+        self.register_buffer('pe', pe.unsqueeze(0))
 
-def plot_positional_encoding(positional_layer):
+    def forward(self, x):
 
-    plt.imshow(positional_layer.pos_enc.detach().numpy().T, cmap='viridis', aspect='auto')
-    plt.colorbar()
-    plt.title("Positional Encoding Matrix")
-    plt.xlabel("Position")
-    plt.ylabel("Dimension")
+        #x = x + (self.pe[:, :x.size()[1], :])
+        x = x + (self.pe[:x.size()[1], :])
 
-    plt.savefig('positional_encoding_matrix.png')
+        return self.dropout(x)
